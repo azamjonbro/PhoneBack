42c7f94e555a13210bb4cf1323ec479e31eaf8d1
azamjonbro <azamjondasturchi11@gmail.com>
2026-07-07 20:40:01 +0500

    fix: update default port from 5000 to 4999

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const connectDB = require('./src/config/db');
 const app = require('./src/app');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4999;
 
 // Connect to Database
 connectDB()
